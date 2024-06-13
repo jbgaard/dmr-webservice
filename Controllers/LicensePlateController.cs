983de4f5ad117173c26c69b657b9a88d7755c83c
jbgaard <jxras11@hotmail.com>
2024-06-13 23:48:24 +0200

    Added in-memory Proxy cache.

--- Controllers/LicensePlateController.cs
+++ Controllers/LicensePlateController.cs
@@ -9,20 +9,39 @@ namespace DMRWebScrapper_service.Controllers
     public class LicensePlateController : ControllerBase
     {
 
+		// Dependency injection of the DMRProxyCache
+		private readonly DMRProxyCache DMRProxyCache;
 
         private readonly ILogger<LicensePlateController> _logger;
 
-        public LicensePlateController(ILogger<LicensePlateController> logger)
-        {
-            _logger = logger;
-        }
+        public LicensePlateController(ILogger<LicensePlateController> logger, DMRProxyCache dmrProxyCache)
+		{
+			_logger = logger;
+			DMRProxyCache = dmrProxyCache;
+		}
 
         [HttpGet("{nummerplade}")]
         public async Task<IActionResult> Get(string nummerplade)
         {
             try
             {
+
+				// Check if the number plate is in the cache
+				Bildata? bildataCache = DMRProxyCache.GetBildataFromCache(nummerplade);
+
+				// If the number plate is in the cache, return the data from the cache
+				if (bildataCache != null)
+				{
+					return Ok(bildataCache);
+				}
+
+				// If the number plate is not in the cache, get the data from the DMRProxy
                 Bildata bildata = await DMRProxy.HentOplysninger(nummerplade, DateTime.Now);
+
+				// Add the data to the cache
+				DMRProxyCache.AddBildataToCache(nummerplade, bildata);
+
+				// Return the data
                 return Ok(bildata);
             }
             catch
